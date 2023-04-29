80b103bc5125097f57bdab775f0549bf7efef684
vlad <vladislavzorin-2003@mail.ru>
2023-04-29 11:45:02 +0300

    First

---
+++ DemoEx/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoEx
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // функция входа 
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Trade123Entities db = new Trade123Entities();
+
+            foreach(User user1 in db.User)
+            {
+                if(user1.UserPassword==TextBox_Password.Text && user1.UserLogin == TextBox_Login.Text)
+                {
+                    MessageBox.Show("Авторизация прошла успешно!");
+                    MainWindow1 wind = new MainWindow1(user1);
+                    wind.Show();
+                    this.Close();
+                    return;
+                }
+            }
+            MessageBox.Show("Вы ввели неверный логин или пароль");
+        }
+
+        private void TextBox_TextChanged_1(object sender, object e)
+        {
+
+        }
+
+        private void Button_Click(object sender, object e)
+        {
+           
+        }
+
+        private void Button_Guest_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+            user.UserName = "Гость";
+            MainWindow1 wind = new MainWindow1(user);
+            wind.Show();
+            this.Close();   
+        }
+    }
+}
---
+++ DemoEx/MainWindow1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemoEx
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow1.xaml
+    /// </summary>
+    public partial class MainWindow1 : Window
+    {
+        public MainWindow1(User user)
+        {
+            InitializeComponent();
+            Trade123Entities db=new Trade123Entities();
+            ProductDb.ItemsSource=db.Product.ToList();
+        }
+    }
+}
